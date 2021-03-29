62ea2cf2a8adb33fe3b4c666c3a17174ff2831c8
tykr0001 <tykr0001@gmail.com>
2021-03-29 14:15:04 +0900

    BOJ

--- BOJ/2104.cpp
+++ BOJ/2104.cpp
@@ -5,61 +5,58 @@ using ll = long long;
 
 int N;
 vector<ll> A;
+vector<ll> prefix_sum;
 
-// @brief       : calculate max score that can exist within range [start, end] by divide and conquer
-// @param start : left most index of subarray
-// @param end   : right most index of subarray
-// @return      : max score
-ll CalcMaxScore(int start, int end) {
-    // basis
-    if (start == end)
-        return A[start] * A[start];
+/**
+ *  @brief  returns max score which is the maximum value of (A[i]+ ... +A[j]) * min({A[i], ..., A[j]})
+ *          when (start <= i <= j <= end) by divide and conquer
+ *  @param  start  left most index of subarray
+ *  @param  end  right most index of subarray
+ *  @return  max score that can exist within range [start, end]
+ */
+ll MaxScore(int start, int end) {
+    if (start == end) return A[start] * A[start];
 
     int mid = (start + end) / 2;
-    int lo  = mid;
-    int hi  = mid + 1;
+    ll left = MaxScore(start, mid);
+    ll right = MaxScore(mid + 1, end);
 
-    ll  left = CalcMaxScore(start, mid);
-    ll right = CalcMaxScore(mid + 1, end);
+    int lo = mid;       // left most index of subarray
+    int hi = mid + 1;   // right most index of subarray
+    ll min_val = min(A[lo], A[hi]);
 
-    // middle      : score in the range inclusive of mid, mid + 1
-    // partial_sum : sum of range [lo, hi]
-    // min_val     : minimum value of range [lo, hi]
-    ll partial_sum = A[lo] + A[hi];
-    ll min_val     = min(A[lo], A[hi]);
-    ll middle      = partial_sum * min_val;
-    
+    // middle is the max score in the range inclusive of mid, mid + 1
+    ll middle = (A[lo] + A[hi]) * min_val;
+
     // calculate max middle score with greedy algorithm using 2-pointer
-    while (start < lo || hi < end) {
-        // search left
-        // when low is not left most and
+    while (!(lo == start && hi == end)) {
+        // search left when low is not left most and
         // high is right most or next low left value is bigger than next right value
-        if (start != lo && (end == hi || A[lo - 1] > A[hi + 1])) {
+        if (lo != start && (hi == end || A[lo - 1] > A[hi + 1])) {
             lo--;
-            partial_sum += A[lo];
             min_val = min(min_val, A[lo]);
         }
-        // search right
-        else {
+        else { // else search right
             hi++;
-            partial_sum += A[hi];
             min_val = min(min_val, A[hi]);
         }
-        middle = max(middle, partial_sum * min_val);
+        middle = max(middle, (prefix_sum[hi] - prefix_sum[lo - 1]) * min_val);
     }
 
     return max({ left, right, middle });
 }
 
 void Solve() {
-    cout << CalcMaxScore(1, N);
+    cout << MaxScore(1, N);
 }
 
 void Init() {
     cin >> N;
     A.resize(N + 1);
+    prefix_sum.resize(N + 1);
     for (int i = 1; i <= N; i++) {
         cin >> A[i];
+        prefix_sum[i] = prefix_sum[i - 1] + A[i];
     }
 }
 
