cbc665e07657fc93a41e496a7309772245a305c9
tykr0001 <54882000+tykr0001@users.noreply.github.com>
2021-10-08 15:08:31 +0900

    Update CpptHeaderTemplate.cpp

--- CpptHeaderTemplate.cpp
+++ CpptHeaderTemplate.cpp
@@ -8,32 +8,47 @@
 *$*       ||        ||     ||   |||  ||   |||   *$*
 *$*                                             *$*
 *$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
-\*************  2021-03-17 23:22:24  *************/
+\*************  2021-10-08 15:04:13  *************/
 
 /*************  C++ Header Template  *************/
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using v2i = vector<vi>;
+using v3i = vector<v2i>;
+using vb = vector<bool>;
+using v2b = vector<vector<bool>>;
+using v3b = vector<v2b>;
+using vl = vector<ll>;
+using v2l = vector<vl>;
+using v3l = vector<v2l>>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define Boost ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define fi first
 #define se second
-#define v vector
-#define vi vector<int> 
-#define vvi vector<vector<int>>
-#define vb vector<bool> 
-#define vvb vector<vector<bool>> 
-#define vs vector<string> 
-#define vc vector<char> 
-#define vl vector<ll> 
-#define vvl vector<vector<ll>> 
-#define pii pair<int,int>
-#define pll pair<ll,ll> 
-#define vpii vector<pair<int,int>>
-#define vpll vector<pair<ll,ll>>
-#define Boost ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define INF INT32_MAX
 #define LINF INT64_MAX
 #define endl '\n'
 #define rep(i,beg,end) for(int i=beg; i<end; i++)
-typedef long long ll;
+template<class T>
+void sort(T& container) { sort(container.begin(), container.end()); }
+template<class T1, typename T2>
+void sort(T1& container, T2 cmp) { sort(container.begin(), container.end(), cmp); }
+template<class T1, class T2>
+istream& operator>>(istream& is, pair<T1, T2>& rhs) { is >> rhs.fi >> rhs.se; return is; }
+template<class T1, class T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& rhs) { os << rhs.fi << ' ' << rhs.se; return os; }
+template<class T>
+istream& operator>>(istream& is, vector<T>& rhs) { for (T& elem : rhs) is >> elem; return is; }
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& rhs) { for (const T& elem : rhs) os << elem << ' '; os << endl; return os; }
+template<class T>
+void resize(T& container, int _size) { container.resize(_size); }
+template<class T, typename... Size>
+void resize(T& container, int _size, Size... _sizes) { container.resize(_size); for (auto& elem : container) resize(elem, _sizes...); }
 /*************************************************/
 
 void Solve(void) {
