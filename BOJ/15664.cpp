a4926c0440f589d6d6e5779b084d87eb35fbbe33
tykr0001 <tykr0001@gmail.com>
2022-03-30 23:37:27 +0900

    BOJ

---
+++ BOJ/15664.cpp
@@ -0,0 +1,99 @@
+/********* github.com/tykr0001/algorithm *********\
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+*$*                                             *$*
+*$*    ||||||||  ||    ||  ||   |||  |||||||    *$*
+*$*       ||      ||  ||   || |||    ||    ||   *$*
+*$*       ||        ||     |||       |||||||    *$*
+*$*       ||        ||     || |||    ||  ||     *$*
+*$*       ||        ||     ||   |||  ||   |||   *$*
+*$*                                             *$*
+*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*$*
+\*************  2022-03-30 22:59:36  *************/
+
+/*************  C++ Header Template  *************/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using v2i = vector<vi>;
+using v3i = vector<v2i>;
+using vb = vector<bool>;
+using v2b = vector<vb>;
+using vs = vector<string>;
+using v2s = vector<vs>;
+using vc = vector<char>;
+using v2c = vector<vc>;
+using vl = vector<ll>;
+using v2l = vector<vl>;
+using v3l = vector<v2l>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define Boost ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define fi first
+#define se second
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3f
+#define endl '\n'
+#define rep(i,beg,end) for(int i=beg; i<end; i++)
+template<class T>
+void sort(T& container) { sort(container.begin(), container.end()); }
+template<class T1, typename T2>
+void sort(T1& container, T2 cmp) { sort(container.begin(), container.end(), cmp); }
+template<class T1, class T2>
+istream& operator>>(istream& is, pair<T1, T2>& rhs) { is >> rhs.fi >> rhs.se; return is; }
+template<class T1, class T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& rhs) { os << rhs.fi << ' ' << rhs.se; return os; }
+template<class T>
+istream& operator>>(istream& is, vector<T>& rhs) { for (T& elem : rhs) is >> elem; return is; }
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& rhs) { for (const T& elem : rhs) os << elem << ' '; os << endl; return os; }
+template<class T>
+void resize(T& container, int _size) { container.resize(_size); }
+template<class T, typename... Size>
+void resize(T& container, int _size, Size... _sizes) { container.resize(_size); for (auto& elem : container) resize(elem, _sizes...); }
+/*************************************************/
+
+int n, m;
+vi arr;
+vb selected;
+v2i ans;
+
+void BackTrack(int idx, int depth) {
+    if (depth == m) {
+        vi tmp;
+        for (int i = 0; i < n; i++) {
+            if (selected[i]) tmp.emplace_back(arr[i]);
+        }
+        ans.emplace_back(tmp);
+        return;
+    }
+    for (int i = idx; i < n; i++) {
+        selected[i] = true;
+        BackTrack(i + 1, depth + 1);
+        selected[i] = false;
+    }
+}
+
+void Solve(void) {
+    BackTrack(0, 0);
+    sort(ans);
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    for(auto vec : ans) 
+        cout << vec;   
+}
+
+void Init(void) {
+    cin >> n >> m;
+    resize(arr, n);
+    resize(selected, n);
+    cin >> arr;
+    sort(arr);
+}
+
+int main(void) {
+    Boost;
+    Init();
+    Solve();
+    return 0;
+}
