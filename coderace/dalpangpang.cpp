02ce1b07c10df0d4105294d0764849a80a0f5758
tykr0001 <tykr0001@gmail.com>
2022-07-20 14:02:17 +0900

    coderace

---
+++ coderace/dalpangpang.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[1000][1000];
+
+void OuterLine() {
+
+}
+
+void InnerLine() {
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> n >> m;
+    
+    return 0;
+}
