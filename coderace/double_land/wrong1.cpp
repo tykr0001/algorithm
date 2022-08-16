3cb18fd8d752716d30afe56f3f3f49e882d216b7
안현기 <54882000+tykr0001@users.noreply.github.com>
2022-08-16 18:35:00 +0900

    coderace

--- coderace/double_land/wrong1.cpp
+++ coderace/double_land/wrong1.cpp
@@ -58,56 +58,77 @@ void resize(T& container, int _size, Size... _sizes) { container.resize(_size); 
 
 int n;
 int arr[1000];
-ll dp_0[1000][2][2]; // {idx,used_0,used_1}
-ll dp_1[1000][2][2]; // {idx,used_0,used_1}
 ll odd_sum[1000];
 ll even_sum[1000];
 
-int DP(int idx, int use, int turn) {
-    dp_0[0][0][0] = arr[0];
-    dp_0[1][1][0] = 2 * (arr[0] + arr[1]);
-    dp_1[0][0][0] = 0;
-    dp_1[1][0][0] = arr[1];
-    dp_1[1][1][0] = 0;
-
-    for (int i = 2; i < n; i++) {
-        for (int j = 0; j < 2; j++) {
-            
-
-            if (j == 0) { // first turn
-                dp_0[i][0][j] = dp_0[i - 1][0][0];
-                dp_0[i][1][j] = dp_0[i - 1][1][0];
-                dp_1[i][0][j] = dp_1[i - 1][0][0];
-                dp_1[i][1][j] = dp_1[i - 1][1][0];
-            }
-            else { // second turn
-                dp_0[i][0][j] = dp_0[i - 1][0][0] + arr[i];
-                dp_0[i][1][j] = max(dp_0[i - 1][1][0], 2 * (dp_0[i - 2][0][0] + arr[i - 1] + arr[i]));
-                dp_1[i][0][j] = dp_1[i - 1][0][0] + arr[i];
-                dp_1[i][1][j] = max(dp_1[i - 1][1][0], 2 * (dp_1[i - 2][0][0] + arr[i - 1] + arr[i]));
-            }
-        }
-    }
-}
 
 void Solve(void) {
+    int ans_A = 0;
+    int ans_B = 0;
+    for(int i=0; i<n-1; i++) {
+        int tmp_A = 0, tmp_B = 0;
+        int max_A = 0, max_B = 0;
+        for(int j=i+2; j<n-1; j+=2) {
+            // A first
+            if(i%2 == 0) {
+                tmp_A = (even_sum[i] + arr[i+1]) * 2;
+                tmp_A += odd_sum[j] - odd_sum[i+1] + even_sum[n-1] - even_sum[j];
+                tmp_B = (odd_sum[i] + even_sum[j] - even_sum[i] + arr[j+1]) * 2;
+                tmp_B += odd_sum[n-1] - odd_sum[j+1];
 
+                if(max_B < tmp_B) {
+                    max_A = tmp_A;
+                    max_B = tmp_B;
+                }
+            }
+            // B first
+            else {
+                tmp_B = (odd_sum[i] + arr[i+1]) * 2;
+                tmp_B += even_sum[j] - even_sum[i+1] + odd_sum[n-1] - odd_sum[j];
+                tmp_A = (even_sum[i] + odd_sum[j] - odd_sum[i] + arr[j+1]) * 2;
+                tmp_A += even_sum[n-1] - even_sum[j+1];
+
+                if(max_A < tmp_A) {
+                    max_A = tmp_A;
+                    max_B = tmp_B;
+                }
+            }
+            // cout << "i: " << i << endl;
+            // cout << "j: " << j << endl;
+            // cout << "tmp_A: " << tmp_A << endl;
+            // cout << "tmp_B: " << tmp_B << endl;
+            if(ans_B < max_B) {
+                ans_A = max_A;
+                ans_B = max_B;
+            }
+            else if(ans_B == max_B) {
+                ans_A = min(ans_A, max_A);
+            }
+            
+            // cout << "ans_A: " << ans_A << endl;
+            // cout << "ans_B: " << ans_B << endl;
+        }
+    }
+
+    cout << ans_A << endl;
 }
 
 void Init(void) {
     cin >> n;
-    memset(dp_0, -1, sizeof(dp_0));
-    memset(dp_1, -1, sizeof(dp_1));
-
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-        if (i % 2)
-            odd_sum[i / 2] += odd_sum[max(i / 2 - 1, 0)] + arr[i];
-        else
-            even_sum[i / 2] += even_sum[max(i / 2 - 1, 0)] + arr[i];
-    }
 
     sort(arr, arr + n, greater<int>());
+    for (int i = 0; i < n; i++) {
+        if (i % 2) {
+            odd_sum[i] += odd_sum[max(i - 1, 0)] + arr[i];
+            odd_sum[min(i+1,n-1)] = odd_sum[i];
+        }
+        else {
+            even_sum[i] += even_sum[max(i - 1, 0)] + arr[i];
+            even_sum[min(i+1,n-1)] = even_sum[i];
+        }
+    }
 }
 
 int main(void) {
